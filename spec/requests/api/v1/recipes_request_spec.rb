a4e0772888469777f0b525f6334cbf5800703e4c
Lauren Golden <llgolden@gmail.com>
2023-08-05 14:11:08 -0600

    Recipe request sad paths

--- spec/requests/api/v1/recipes_request_spec.rb
+++ spec/requests/api/v1/recipes_request_spec.rb
@@ -33,5 +33,65 @@ RSpec.describe "get recipes", type: :request do
       expect(recipes[:data].first[:attributes]).to_not have_key(:cuisineType)
       expect(recipes[:data].first[:attributes]).to_not have_key(:dishType)
     end
+
+    xit "returns a list of recipes for a random country when country is not specified" do
+      get "/api/v1/recipes"
+
+      expect(response).to be_successful
+
+      recipes = JSON.parse(response.body, symbolize_names: true)
+
+      expect(recipes).to be_a(Hash)
+      expect(recipes).to have_key(:data)
+      expect(recipes[:data]).to be_an(Array)
+      expect(recipes[:data].first).to have_key(:id)
+      expect(recipes[:data].first[:id]).to be(nil)
+      expect(recipes[:data].first).to have_key(:type)
+      expect(recipes[:data].first[:type]).to eq("recipe")
+      expect(recipes[:data].first).to have_key(:attributes)
+      expect(recipes[:data].first[:attributes]).to be_a(Hash)
+      expect(recipes[:data].first[:attributes]).to have_key(:title)
+      expect(recipes[:data].first[:attributes][:title]).to be_a(String)
+      expect(recipes[:data].first[:attributes]).to have_key(:url)
+      expect(recipes[:data].first[:attributes][:url]).to be_a(String)
+      expect(recipes[:data].first[:attributes]).to have_key(:country)
+      expect(recipes[:data].first[:attributes][:country]).to be_a(String)
+      expect(recipes[:data].first[:attributes][:country]).to eq("thailand")
+      expect(recipes[:data].first[:attributes]).to have_key(:image)
+      expect(recipes[:data].first[:attributes][:image]).to be_a(String)
+
+      expect(recipes[:data].first[:attributes]).to_not have_key(:healthLabels)
+      expect(recipes[:data].first[:attributes]).to_not have_key(:calories)
+      expect(recipes[:data].first[:attributes]).to_not have_key(:cuisineType)
+      expect(recipes[:data].first[:attributes]).to_not have_key(:dishType)
+    end
+
+    it "returns an empty array when there are no search matches" do
+      get "/api/v1/recipes?country=notarealcountry"
+
+      expect(response).to be_successful
+
+      recipes = JSON.parse(response.body, symbolize_names: true)
+
+      expect(recipes).to be_a(Hash)
+      expect(recipes).to have_key(:data)
+      expect(recipes[:data]).to be_an(Array)
+      expect(recipes[:data]).to eq([])
+      expect(recipes[:data].count).to eq(0)
+    end
+
+    it "returns an empty array when the country parameter is an empty string" do
+      get "/api/v1/recipes?country=''"
+
+      expect(response).to be_successful
+
+      recipes = JSON.parse(response.body, symbolize_names: true)
+
+      expect(recipes).to be_a(Hash)
+      expect(recipes).to have_key(:data)
+      expect(recipes[:data]).to be_an(Array)
+      expect(recipes[:data]).to eq([])
+      expect(recipes[:data].count).to eq(0)
+    end
   end
 end
