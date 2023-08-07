6915f9742a82b64c6fe5843cfb83d6cee3af577a
Lauren Golden <llgolden@gmail.com>
2023-08-07 10:24:24 -0600

    Add extra countries for happy path testing

--- spec/requests/api/v1/air_quality_spec.rb
+++ spec/requests/api/v1/air_quality_spec.rb
@@ -29,6 +29,60 @@ RSpec.describe "get air quality", type: :request do
       expect(aq_data[:data][:attributes][:co_concentration]).to be_a(Float)
     end
 
+    it "returns air quality data for a given country's capital city" do
+      get "/api/v1/air_quality?country=France"
+
+      expect(response).to be_successful
+
+      aq_data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(aq_data).to be_a(Hash)
+      expect(aq_data).to have_key(:data)
+      expect(aq_data[:data]).to be_a(Hash)
+      expect(aq_data[:data]).to have_key(:id)
+      expect(aq_data[:data][:id]).to eq("null")
+      expect(aq_data[:data]).to have_key(:type)
+      expect(aq_data[:data][:type]).to eq("air_quality")
+      expect(aq_data[:data]).to have_key(:attributes)
+      expect(aq_data[:data][:attributes]).to be_a(Hash)
+      expect(aq_data[:data][:attributes]).to have_key(:city)
+      expect(aq_data[:data][:attributes][:city]).to be_a(String)
+      expect(aq_data[:data][:attributes][:city]).to eq("Paris")
+      expect(aq_data[:data][:attributes]).to have_key(:aqi)
+      expect(aq_data[:data][:attributes][:aqi]).to be_an(Integer)
+      expect(aq_data[:data][:attributes]).to have_key(:pm25_concentration)
+      expect(aq_data[:data][:attributes][:pm25_concentration]).to be_a(Float)
+      expect(aq_data[:data][:attributes]).to have_key(:co_concentration)
+      expect(aq_data[:data][:attributes][:co_concentration]).to be_a(Float)
+    end
+
+    it "returns air quality data for a given country's capital city" do
+      get "/api/v1/air_quality?country=Cuba"
+
+      expect(response).to be_successful
+
+      aq_data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(aq_data).to be_a(Hash)
+      expect(aq_data).to have_key(:data)
+      expect(aq_data[:data]).to be_a(Hash)
+      expect(aq_data[:data]).to have_key(:id)
+      expect(aq_data[:data][:id]).to eq("null")
+      expect(aq_data[:data]).to have_key(:type)
+      expect(aq_data[:data][:type]).to eq("air_quality")
+      expect(aq_data[:data]).to have_key(:attributes)
+      expect(aq_data[:data][:attributes]).to be_a(Hash)
+      expect(aq_data[:data][:attributes]).to have_key(:city)
+      expect(aq_data[:data][:attributes][:city]).to be_a(String)
+      expect(aq_data[:data][:attributes][:city]).to eq("Havana")
+      expect(aq_data[:data][:attributes]).to have_key(:aqi)
+      expect(aq_data[:data][:attributes][:aqi]).to be_an(Integer)
+      expect(aq_data[:data][:attributes]).to have_key(:pm25_concentration)
+      expect(aq_data[:data][:attributes][:pm25_concentration]).to be_a(Float)
+      expect(aq_data[:data][:attributes]).to have_key(:co_concentration)
+      expect(aq_data[:data][:attributes][:co_concentration]).to be_a(Float)
+    end
+
     xit "will give you air quality information for a random country if no country parameter is passed in" do
       get "/api/v1/air_quality?country=random"
 
